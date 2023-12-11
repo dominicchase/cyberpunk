9dc058089f69b0acbf51274bc188fe78e52cd1fc
hitmonchan <dominiccpeterson@gmail.com>
2023-12-10 23:27:09 -0500

    began bg animations

---
+++ app/src/pages/Homepage/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export const Homepage = () => {
+  // 洋服屋
+  return (
+    <>
+      <div className="bg"></div>
+      <img src="/public/brand.png" className="test" />
+    </>
+  );
+};
